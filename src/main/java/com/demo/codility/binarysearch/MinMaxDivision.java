c3b9eab4a55ea76d3acb013856e768dafcf2ec5b
kyoungjin.lee <kyoungjin.lee@linefriends.com>
2019-02-12 15:16:20 +0900

    prime and composite number - prob1

---
+++ src/main/java/com/demo/codility/binarysearch/MinMaxDivision.java
@@ -0,0 +1,25 @@
+package com.demo.codility.binarysearch;
+
+public class MinMaxDivision {
+    public static void main(String[] args){
+        int[] A = {4, 4, 5, 5, 1};
+        int K = 0;
+        int M = 0;
+        int result = solution(K, M, A);
+    }
+
+    private static int solution(int K, int M, int[] A) {
+
+        int pivot = A[A.length/2];
+
+        for(int i =0; i<A.length; i++){
+            if( pivot > A[i]){
+
+            }
+        }
+
+
+        return 0;
+
+    }
+}
---
+++ src/main/java/com/demo/codility/lesson10/CountFactors.java
@@ -0,0 +1,47 @@
+package com.demo.codility.lesson10;
+
+import java.util.*;
+
+/*
+ * Prime And Composite Number.
+ * Prime Number: 소수: 1과 자기 자신의 곱으로만 자신을 표현할 수 있는 수 (ex. 3 = 1 x 3 , 5 = 1 x 5, ..)
+ * Composite Number: 합성수: 1과 자기 자신이 아닌 다른 자연수의 곱으로 나타낼 수 있는 자연수.
+ * 소인수분해= 약수 구하기.
+ * 약수의 개수 == 소인수분해한 결과, (지수+1) * (지수+1) * ..
+ */
+public class CountFactors {
+
+    public static void main(String[] args){
+
+        int result =  solution(2);
+        System.out.println("result: " + result);
+    }
+
+    public static int solution(int N){
+
+
+        int num = N;
+        int d = 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        while(num != 1){
+
+            if( num % d  != 0 ) d++;
+
+            if( num % d  == 0 ) {
+                num = num/d;
+
+                if ( map.containsKey(d) ) map.put(d, map.get(d) + 1);
+                else map.put(d, 1);
+            }
+        }
+
+        int count = 1;
+        Iterator<Integer> it =  map.keySet().iterator();
+        while(it.hasNext()){
+            int key = it.next();
+            count = count * (map.get(key) + 1);
+        }
+
+        return count;
+    }
+}
